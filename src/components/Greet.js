a305974722773a2fbee8b99f832d4c9c34b000c8
Vaisakh M <1306@trenser.com>
2022-03-28 17:04:48 +0530

    first commit

---
+++ src/components/Greet.js
@@ -0,0 +1,22 @@
+// function components
+/* 
+Simple functions
+user Func components as much as possible
+Absence of this keyword
+Soluction without using state
+Mainly responsible for the UI 
+Sataeless/Dumb/Presentatial*/
+import React from "react"; 
+
+
+// function Greet(){
+//     return <h1> Hello Vaisakh</h1>
+// }
+// arrow function
+const Greet = () => <h1>Hello Vaisakh</h1>
+
+export default Greet
+
+// named export
+// export const Greet = () => <h1>Hello Vaisakh</h1>
+// in app component we need to import with Greet and in side a {Greet}
